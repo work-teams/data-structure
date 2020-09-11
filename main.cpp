aa0629cfd822e328a2c66e96899ae7e167504627
Jorge Marin <jorgeluis.marin@outlook.com>
2020-09-11 07:38:14 -0500

    Agrega consola improvisada para test
    -esto se hace para poder probar el correcto funcionamiento de las clases
     y metodos implementados.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,18 @@
+/*                           Comentario
+---------------------------------------------------------------------
+	University: Universidad Nacional Mayor de San Marcos
+	Faculty: Ingenieria de Sistemas e Informatica
+	School: Ingenieria de Software
+	
+	Subject: Estructura de Datos
+	Project: Sistema Banco de Sangre
+	
+	Name: Marin Evangelista Jorge Luis
+	Date: 2020/09/11
+	User: jm-dev7
+---------------------------------------------------------------------
+*/
+
 #include <iostream>
 
 #include "include/Archivo.h"
@@ -5,10 +20,75 @@
 #include "include/linkedList.h"
 #include "include/Validar.h"
 
-using namespace std;
+using namespace ::std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+/*                        Funcion Principal
+---------------------------------------------------------------------*/
+int main(int argc, char** argv) {
+	cout << "EstructuraDatos_ProyectoFinal_SistemaBancoSangre" << endl << endl;
+	
+	/*---------------------------------------------*/
+	Lista lista;
+	Paciente paciente;
+	fstream archivo;
+	
+	string codigo;
+	string tipoSangre;
+	string edad;
+	string prioridad;
+	string sexo;
+	string nombre;
+
+	Nodo* buscado;
+	
+	int op;
+	
+	/*---------------------------------------------*/
+	recuperar_datos(archivo, lista, paciente); //recupera datos del archivo
+
+	do {
+		cin >> op;
+		switch (op) {
+			case 1:
+				codigo = validar_codigo(codigo);
+				nombre = validar_nombre();
+				edad = validar_edad(edad);
+				sexo = validar_sexo(sexo);
+				tipoSangre = validar_tipoSangre(tipoSangre);
+				prioridad = define_prioridad(tipoSangre);
+
+				paciente = generar_paciente(codigo, nombre, edad, sexo, tipoSangre, prioridad);
+				
+				insertar_aLista(lista, paciente); //inserta en lista enlazada
+				cout << "insertado\n";
+				break;
+			case 2:
+				guardar_datos(archivo, lista); //guarda datos en archivo
+				break;
+			case 3:
+				mostrar_lista(lista); break; //imprime la lista enlazada
+			case 5:
+				destruir_lista(lista); break;
+			case 6:
+				buscado = buscar_codigo(lista, codigo);
+				if (buscado != nullptr) {
+					eliminar_nodo(lista, buscado);
+					cout << "eliminado\n";
+				}
+				break;
+			case 7:
+				ordenar_prioridad(lista);
+				break;
+			case 8:
+				buscado = buscar_codigo(lista, codigo);
+				if (buscado != nullptr) {
+					imprimir_paciente(buscado->paciente);
+				}
+				break;
+		}
+	} while (op != 4);
+	
+	return 0;
 }
