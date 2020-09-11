2c2c5a02ca804a73c0f4b45cde520c147361f3df
Jorge Marin <jorgeluis.marin@outlook.com>
2020-09-11 10:40:05 -0500

    Agrega clase Consola con sus metodos
    -implementa metodo menuPrincipal
    -implementa metodo menuMostrar
    -implementa metodo menuInsertar

---
+++ include/Consola.h
@@ -0,0 +1,15 @@
+#ifndef CONSOLA_H
+#define CONSOLA_H
+
+#include <iostream>
+using namespace ::std;
+
+/*                            Declaracion
+---------------------------------------------------------------------*/
+void menuPrincipal();
+
+void menuMostrar();
+
+void menuInsertar();
+
+#endif
---
+++ src/Consola.cpp
@@ -0,0 +1,36 @@
+#include "Consola.h"
+
+/*                            Definicion
+---------------------------------------------------------------------*/
+void menuPrincipal() {
+	cout<<"**********************************************";
+	cout<<"\n\t\t MENU PRINCIPAL \n";
+	cout<<"**********************************************\n";
+	cout<<" 	1. Insertar Paciente                      "<<endl;
+	cout<<" 	2. Guardar Datos                          "<<endl;
+	cout<<" 	3. Mostrar Lista                          "<<endl;
+	cout<<" 	4. Salir		                          "<<endl;
+	cout<<" 	5. Eliminar Lista                         "<<endl;
+	cout<<" 	6. Eliminar por Codigo                    "<<endl;
+	cout<<" 	7. Ordenar Lista	                      "<<endl;
+	cout<<" 	8. Buscar por Codigo                      "<<endl;
+	cout<<"**********************************************\n";
+	
+	cout<<"\n INGRESE OPCION: ";
+}
+
+/*---------------------------------------------*/
+void menuMostrar() {
+	cout<<"\n________________________________________________________________________________\n";
+	cout<<" CODIGO		MONBRE PACIENTE 				EDAD 	GENERO	 ";
+	cout<<"\n________________________________________________________________________________\n";
+		
+}
+
+/*---------------------------------------------*/
+void menuInsertar() {
+	cout<<"**********************************************";
+	cout<<"\n\t\t INSERTAR \n";
+	cout<<"**********************************************\n";
+		
+}
